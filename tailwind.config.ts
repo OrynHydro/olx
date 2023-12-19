841b1321c1aafc9aa689be67f01d3b1ae1bc4d3c
orynhydro <oreshnikovvl@gmail.com>
2023-12-19 20:32:24 +0200

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,18 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				cyan: '#002f34',
+				gray: '#7F9799',
+			},
+		},
+	},
+	plugins: [],
 }
 export default config
