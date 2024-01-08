59ff6460734607c03aa154eb80c40561023c03b3
orynhydro <oreshnikovvl@gmail.com>
2024-01-08 22:23:49 +0200

    adding schema

---
+++ client/src/libs/schemas/adding.schema.ts
@@ -0,0 +1,56 @@
+import { z } from 'zod'
+
+export const addingSchema = z.object({
+	title: z
+		.string()
+		.min(16, {
+			message: 'У заголовку має бути не менше 16 символів',
+		})
+		.max(70, {
+			message: 'У заголовку може бути не більше 70 символів',
+		}),
+	photo: z.array(z.string()).min(1, {
+		message: 'Додайте хоча б одне фото',
+	}),
+	desc: z
+		.string()
+		.min(40, {
+			message: 'Опис повинен бути не коротшим за 40 знаків',
+		})
+		.max(9000, {
+			message: 'Опис повинен бути не довшим за 9000 знаків',
+		}),
+	price: z
+		.number({
+			invalid_type_error:
+				'Будь ласка, вкажіть ціну. Зверніть увагу, що вказувати нереальні ціни або умови заборонено.',
+		})
+		.optional(),
+	sellerType: z.enum(['private', 'business']),
+	condition: z.enum(['utilized', 'new']),
+	autoRenewal: z.boolean(),
+	location: z.string().min(1, {
+		message: 'Будь ласка, вкажіть місце знаходження',
+	}),
+	sellerName: z.string().min(1, {
+		message: "Будь ласка, вкажіть ім'я контактної особи",
+	}),
+	sellerEmail: z
+		.string()
+		.email({
+			message: 'Це не схоже на електронну пошту',
+		})
+		.optional(),
+	sellerPhone: z.string().refine(
+		value => {
+			const phoneNumberRegex = /^380\d{9}$/
+			return phoneNumberRegex.test(value)
+		},
+		{
+			message:
+				'Будь ласка, введіть коректний номер телефону у форматі: 380ХХХХХХХХХ.',
+		}
+	),
+})
+
+export type TAddingSchema = z.infer<typeof addingSchema>
